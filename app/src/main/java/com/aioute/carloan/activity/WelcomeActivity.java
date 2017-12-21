5246e7f4c1562df9d9330631cca1fb1059263d90
Administrator <271627317@qq.com>
2017-12-21 23:22:11 +0800

    增加TextView功能

--- app/src/main/java/com/aioute/carloan/activity/WelcomeActivity.java
+++ app/src/main/java/com/aioute/carloan/activity/WelcomeActivity.java
@@ -1,13 +1,18 @@
 package com.aioute.carloan.activity;
 
+import android.text.Html;
 import android.view.Menu;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.aioute.carloan.R;
 import com.aioute.carloan.base.CustomBaseActivity;
 
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
+
+import cn.sft.util.Util;
+import cn.sft.view.AlwaysMarqueeTextView;
 
 /**
  * Created by Administrator on 2017/12/19.
@@ -19,10 +24,26 @@ public class WelcomeActivity extends CustomBaseActivity {
     @ViewById(R.id.image)
     ImageView imageView;
 
+    @ViewById(R.id.checkBox)
+    AlwaysMarqueeTextView textView;
+
     @Override
     protected void afterViews() {
         imageView.setImageDrawable(getResources().getDrawable(R.mipmap.ic_launcher_round));
-    }
+
+        textView.setSingleLine(false);
+        textView.setLineSpacing(Util.dp2px(this, 2), 1.2f);
+        textView.setText(Html.fromHtml("<font size=\"6\" color=\"red\">今天天气好吗今天天气好吗？</font><br><font size=\"3\" color=\"green\">挺好的</font>"));
+
+//    public void setText(CharSequence text, TextView.BufferType type) {
+//        setText(text, type, true, 0);
+//
+//        if (mCharWrapper != null) {
+//            mCharWrapper.mChars = null;
+//        }
+//    }
+
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
